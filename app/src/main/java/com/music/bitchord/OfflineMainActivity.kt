8dfa7bf07fe803bffa58d06c6b82b0d1bf03a907
eaudesauvage <networkplaysleep@gmail.com>
2026-09-09 19:21:58 +0530

    Use Compose BackHandler for offline player navigation

--- app/src/main/java/com/music/bitchord/OfflineMainActivity.kt
+++ app/src/main/java/com/music/bitchord/OfflineMainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.BackHandler
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
